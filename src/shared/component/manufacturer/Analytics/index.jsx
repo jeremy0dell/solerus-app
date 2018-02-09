50a58796224c388dccfbe8b14e4de0462d85b454
Jeremy Odell <jeremy0dell@yahoo.com>
2018-02-09 15:14:36 -0600

    remove .jsx

--- src/shared/component/manufacturer/Analytics/index.jsx
+++ src/shared/component/manufacturer/Analytics/index.jsx
@@ -5,7 +5,7 @@ import { getAnalytics } from '../../../util/analytics'
 import { getProducts } from '../../../util/getters'
 
 import TopBar from './TopBar'
-import Product from './Product.jsx' // eslint-disable-line
+import Product from './Product' // eslint-disable-line
 import Dropdowns from './Dropdowns'
 
 // I need to get the associated Products
