a8031d08e02e11b4979f162589efdee8062ee10b
Jeremy Odell <jeremy0dell@yahoo.com>
2019-12-23 16:37:51 -0500

    Update routing.js

--- src/server/routing.js
+++ src/server/routing.js
@@ -19,44 +19,31 @@ import renderApp from './render-app'
 
 export default (app: Object) => {
   app.get(HOME_PAGE_ROUTE, (req, res) => {
-    console.log('got', HOME_PAGE_ROUTE, 'sending', 'res.send(renderApp(req.url, loginPage()))')
     res.send(renderApp(req.url, populateUser(req.user)))
   })
 
   app.get(DASHBOARD_PAGE_ROUTE, (req, res) => {
-    console.log('got', DASHBOARD_PAGE_ROUTE, 'sending', req.user._id.toString())
     fetcher.get(`/api/users/${req.user._id.toString()}`)
     .then((response) => {
-      console.log('response is', response.data)
       res.send(renderApp(req.url, populateUser(response.data)))
     })
     .catch(console.error)
   })
 
   app.get(MANUFACTURER_LOGIN_ROUTE, (req, res) => {
-    console.log('got1', MANUFACTURER_DASHBOARD_ROUTE, 'sending', req.user)
-    // fetcher.get(`/manu/users/${req.user._id.toString()}`)
-    // .then((response) => {
-    //   console.log('response is', response.data)
-    //   res.send(renderApp(req.url, populateUser(response.data)))
-    // })
     res.send(renderApp(req.url, populateUser(req.user)))
   })
 
   app.get(MANUFACTURER_DASHBOARD_ROUTE, (req, res) => {
-    console.log('got2', MANUFACTURER_DASHBOARD_ROUTE, 'sending', req.user._id.toString())
     fetcher.get(`/manu/${MANUFACTURER_INDEX}/${req.user._id.toString()}`)
     .then((response) => {
-      console.log('response is', response.data)
       res.send(renderApp(req.url, populateUser(response.data)))
     })
   })
 
   app.get(`${MANUFACTURER_INDEX}*`, (req, res) => {
-    console.log('got3', MANUFACTURER_DASHBOARD_ROUTE, 'sending', req.user._id.toString())
     fetcher.get(`/manu/${MANUFACTURER_INDEX}/${req.user._id.toString()}`)
     .then((response) => {
-      console.log('response is', response.data)
       res.send(renderApp(req.url, populateUser(response.data)))
     })
   })
